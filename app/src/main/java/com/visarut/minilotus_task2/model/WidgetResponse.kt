d73c7d7ae90ce1f61d06cdcd8165faf17e7e7fe7
Visarut Techatanchai <61011358@kmitl.ac.th>
2021-08-11 15:22:09 +0700

    Epoxy Android Lib

---
+++ app/src/main/java/com/visarut/minilotus_task2/model/WidgetResponse.kt
@@ -0,0 +1,45 @@
+package com.visarut.minilotus_task2.model
+
+import com.google.gson.annotations.SerializedName
+
+data class WidgetResponse(
+
+	@field:SerializedName("data")
+	val data: Data? = null,
+
+	@field:SerializedName("status")
+	val status: Status? = null
+)
+
+data class Data(
+
+	@field:SerializedName("seeAllText")
+	val seeAllText: String? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null,
+
+	@field:SerializedName("featureBrands")
+	val featureBrands: List<FeatureBrandsItem?>? = null,
+
+	@field:SerializedName("url")
+	val url: String? = null
+)
+
+data class Status(
+
+	@field:SerializedName("code")
+	val code: Int? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null
+)
+
+data class FeatureBrandsItem(
+
+	@field:SerializedName("image")
+	val image: String? = null,
+
+	@field:SerializedName("url")
+	val url: String? = null
+)
